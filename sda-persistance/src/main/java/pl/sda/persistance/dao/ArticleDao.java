ef2fc37f13acf516cbc64ecb315193ce6d923ffc
lkre <lukasz.res@gmail.com>
2019-11-17 11:20:59 +0100

    dodanie listy artykulow w index.jsp

--- sda-persistance/src/main/java/pl/sda/persistance/dao/ArticleDao.java
+++ sda-persistance/src/main/java/pl/sda/persistance/dao/ArticleDao.java
@@ -1,9 +1,12 @@
 package pl.sda.persistance.dao;
 
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 import pl.sda.persistance.HibernateUtil;
 import pl.sda.persistance.entities.TbArticle;
 import pl.sda.persistance.entities.TbUser;
+
+import java.util.List;
 
 public class ArticleDao {
 
@@ -16,6 +19,15 @@ public class ArticleDao {
             tbArticle.setUser(user);
             session.save(tbArticle);
             session.getTransaction().commit();
+        }
+    }
+
+    public List getArticles() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            Query query = session.createQuery("select o from TbArticle o");
+            session.getTransaction().commit();
+            return query.getResultList();
         }
     }
 }
